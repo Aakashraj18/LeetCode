2be420cf6b6724eca7200beafbaae3875cb4e32e
Aakash Kumar Rajak <akashkrrajak18@gmail.com>
2026-07-09 11:33:23 +0530

    Time: 71 ms (21.37%), Space: 245.6 MB (12.6%) - LeetHub

---
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,57 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1,0);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findParent(int node){
+        if(node == parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if(ulp_u == ulp_v) return;
+        if(size[ulp_u] > size[ulp_v]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+        else if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else{
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        DisjointSet ds(n);
+        vector<bool> ans;
+
+        for(int i=1;i<n;i++){
+            if(abs(nums[i] - nums[i-1]) <= maxDiff) ds.unionBySize(i,i-1);
+        }
+
+        for(auto it: queries){
+            int u=it[0], v=it[1];
+            if(ds.findParent(u) == ds.findParent(v)) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
